c8842c46b09d253f7cc0f009ecfec11625cbee32
Lucas Levit <lucassebas96@gmail.com>
2022-07-10 18:45:34 -0300

    subida de fetch

--- js/main.js
+++ js/main.js
@@ -1,63 +1,20 @@
 const formulario = document.getElementById("formulario");
 const btn = document.querySelector(".btn");
 const cards = document.querySelector(".cards");
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
+const telefono = document.getElementById("telefono")
+const mensaje = document.getElementById("mensaje")
+let mesElegido = ""
 
-
-let caracteristicas = [{
-        id: 1,
-        nombre: "Consultorio basico",
-        imagen: "./img/consultorio1.jpg",
-        descripcion: "Este consultorio, cuenta con un sillon",
-        precio: 1300,
-        alquiler: "p/hora",
-        disponible: true,
-        meses: ["marzo", "junio", "agosto","diciembre"],
-    },
-    {
-        id: 2,
-        nombre: "Consultorio medio",
-        imagen: "./img/consultorio2.jpg",
-        descripcion: "Este consultorio, cuenta con un sillon y escritorio",
-        precio: 1500,
-        alquiler: "p/hora",
-        disponible: true,
-        meses: ["enero", "abril", "septiembre"],
-    },
-    {
-        id: 3,
-        nombre: "Consultorio Medio Mejorado",
-        imagen: "./img/consultorio1.jpg",
-        descripcion: "Este consultorio, cuenta con un divan y un escritorio",
-        precio: 1800,
-        alquiler: "p/hora",
-        disponible: true,
-        meses: ["febrero", "mayo", "julio", "septiembre"],
-    },
-    {
-        id: 4,
-        nombre: "Consultorio premium",
-        imagen: "./img/consultorio3.jpg",
-        descripcion: "Este consultorio, cuenta con un sillon y divan",
-        precio: 2000,
-        alquiler: "p/hora",
-        disponible: true,
-        meses: ["octubre", "noviembre"],
-    },
-    {
-        id: 5,
-        nombre: "Consultorio premium mejorado",
-        imagen: "./img/consultorio3.jpg",
-        descripcion: "Este consultorio, cuenta con un sillon, un divan y un escritorio",
-        precio: 2300,
-        alquiler: "p/hora",
-        disponible: true,
-        meses: ["diciembre"]
-    },
-]
+const datos = async () => {
+    let respuesta = await fetch("../baseDeDatos/datos.json")
+    return respuesta.json()
+}
 
 
 function renderizarTarjeta(consultorio) {
-    let {imagen, nombre, descripcion, precio, alquiler} = consultorio
+    let {imagen, nombre, descripcion, precio, alquiler,id} = consultorio
     const plantillaTarjeta =`
             <div class="card">
             <img src="${imagen}" class="card-img-top" alt="...">
@@ -65,7 +22,7 @@ function renderizarTarjeta(consultorio) {
                 <h5 class="card-title">${nombre}</h5>
                 <p class="card-text">${descripcion}</p>
                 <span>$${precio} ${alquiler}</span>
-                <a href="#" class="btn btn-primary">Reservar</a>
+                <a href="#" class="btn btn-primary" id="reservar" data-id="${id}">Reservar</a>
             </div>
         </div>
     `
@@ -83,7 +40,7 @@ contenedorTarjetas.innerHTML += tarjeta
 })
 }
 const mesFiltro = document.getElementById("mesFiltro")
-let mesElegido = ""
+
 
 mesFiltro.addEventListener ("change", (e)=>{
     if (e.target.value !== "--") {
@@ -91,32 +48,14 @@ mesFiltro.addEventListener ("change", (e)=>{
     }
 })
 
-formulario.addEventListener("submit" ,(e)=>{
+formulario.addEventListener("submit" ,async(e)=>{
     e.preventDefault()
-    const nombre = document.getElementById("nombre")
-    const email = document.getElementById("email")
-    const telefono = document.getElementById("telefono")
-    const mensaje = document.getElementById("mensaje")
-
+    let caracteristicas =  await datos()
     const camposVacios = nombre.value && email.value && telefono.value && mensaje.value
     if (!camposVacios) {swal("Por favor complete los campos vacios");}
     else {
     const consultoriosFiltrado = caracteristicas.filter(consultorio => consultorio.meses.includes(mesElegido))
     renderizarFiltro(consultoriosFiltrado);
-    const datosUsuario = {nombre:nombre.value, email:email.value, telefono:telefono.value, mes:mesElegido, mensaje:mensaje.value}
-    console.log(datosUsuario);
-    localStorage.setItem("datosUsuario", JSON.stringify (datosUsuario))
-    Toastify({
-        text: "Gracias por tus datos, te contactaremos a la brevedad",
-        duration: 3000,
-        newWindow: true,
-        close: true,
-        gravity: "bottom", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {background: "rgba(99, 28, 42, 0.7)"},
-        onClick: function(){} // Callback after click
-        }).showToast();
     }
 })
 
@@ -125,4 +64,43 @@ function obtenerDatos () {
     console.log(persona);
 }
 obtenerDatos()
+
+contenedorTarjetas.addEventListener ("click" , async (e)=>{
+    if (e.target.id === "reservar") {
+        let id = e.target.dataset.id
+        let caracteristicas =  await datos()
+        const consultoriosFiltrado = await caracteristicas.find(consultorio => consultorio.id === parseInt(id))
+        console.log(consultoriosFiltrado);
+const datosUsuario = {
+    nombre:nombre.value, 
+    email:email.value,
+    telefono:telefono.value,
+    mes:mesElegido,
+    mensaje:mensaje.value,
+    nombreConsultorio:consultoriosFiltrado.nombre,
+    imagenConsultorio:consultoriosFiltrado.imagen,
+    descripcionConsultorio:consultoriosFiltrado.descripcion,
+    precioConsultorio: consultoriosFiltrado.precio,
+    alquilerConsultorio: consultoriosFiltrado.alquiler,
+
+}
+
+console.log(datosUsuario);
+localStorage.setItem("datosUsuario", JSON.stringify (datosUsuario))
+Toastify({
+    text: "Gracias por tus datos, te contactaremos a la brevedad",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {background: "rgba(99, 28, 42, 0.7)"},
+    onClick: function(){} // Callback after click
+    }).showToast();
+    setTimeout(()=>{
+        window.location.href = "../html/reserva.html"
+    },1500)
+    }
+} )
 
---
+++ js/reserva.js
@@ -0,0 +1,36 @@
+const datosReserva = JSON.parse(localStorage.getItem("datosUsuario"))
+const contenedorReserva = document.getElementById ("reserva")
+const contenedorUsuario = document.getElementById ("usuario")
+const btnfinalizar = document.getElementById ("finalizar")
+
+window.addEventListener ("DOMContentLoaded", rendeReserva)
+function rendeReserva (){
+    const { nombreConsultorio, imagenConsultorio, descripcionConsultorio, precioConsultorio, alquilerConsultorio, email, mensaje, mes, telefono, nombre } = datosReserva
+    console.log(nombreConsultorio);
+    contenedorReserva.innerHTML =`
+            <div class="card">
+            <img src="${imagenConsultorio}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title" id="name">${nombreConsultorio}</h6>
+                <p class="card-text">${descripcionConsultorio}</p>
+                <span>$${precioConsultorio} ${alquilerConsultorio}</span>
+            </div>
+        </div>
+    `
+    contenedorUsuario.innerHTML = `
+            <p> Nombre y apellido: ${nombre}</p>
+            <p> Email: ${email}</p>
+            <p> Telefono: ${telefono}</p>
+            <p> Mes de reserva: ${mes}</p>
+            <p> Mensaje: ${mensaje}</p>
+    `
+}
+btnfinalizar.addEventListener ("click" , ()=> {
+    localStorage.removeItem("datosUsuario")
+    setTimeout(()=>{
+        window.location.href = "../index.html"
+    },1500)
+})
+
+console.log(datosReserva);
+console.log(contenedorReserva);
